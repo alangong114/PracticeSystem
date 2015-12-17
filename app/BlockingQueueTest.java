b6d0d7fe8c9b8a8df9fe53a27da08d5169ca0a10
alangong114 <alangong456@qq.com>
2015-12-17 15:45:48 +0800

    Create BlockingQueueTest.java

---
+++ app/BlockingQueueTest.java
@@ -0,0 +1,17 @@
+package app;
+import java.util.concurrent.LinkedBlockingQueue; //LinkedBlockingDeque
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+ public class BlockingQueueTest{
+      public static void main(String[] args){
+    
+      }
+ 
+  
+      public static void LinkedBlockingQueueTest(){
+      
+      }
+      public static void ArrayBlockingQueueTest(){
+      
+      }
+  }
