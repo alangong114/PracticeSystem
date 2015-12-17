dde01646efb0e16976053427de7b6524ddd5e5f6
alangong114 <alangong456@qq.com>
2015-12-17 15:49:51 +0800

    Update BlockingQueueTest.java

--- app/BlockingQueueTest.java
+++ app/BlockingQueueTest.java
@@ -6,12 +6,37 @@ import java.util.concurrent.BlockingQueue;
       public static void main(String[] args){
     
       }
- 
   
-      public static void LinkedBlockingQueueTest(){
-      
-      }
+  /**
+  * 阻塞栈
+  * 
+  * @author
+  * 
+  */
+
       public static void ArrayBlockingQueueTest(){
-      
+        BlockingQueue queue = new ArrayBlockingQueue(20);
+        for (int i = 0; i < 30; i++) {
+            // 将指定元素添加到此队列中，如果没有可用空间，将一直等待（如果有必要）。
+            queue.put(i);
+            System.out.println("向阻塞队列中添加了元素:" + i);
+        }
+        System.out.println("程序到此运行结束，即将退出----");
+  
+      }
+      /**
+       * 阻塞队列
+       * 
+       * 
+      */
+      public static void LinkedBlockingQueueTest(){
+          BlockingDeque bDeque = new LinkedBlockingDeque(20);
+        for (int i = 0; i < 30; i++) {
+            // 将指定元素添加到此阻塞栈中，如果没有可用空间，将一直等待（如果有必要）。
+            bDeque.putFirst(i);
+            System.out.println("向阻塞栈中添加了元素:" + i);
+        }
+        System.out.println("程序到此运行结束，即将退出----");
+
       }
   }
